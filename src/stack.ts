ec7fee7e76304f0b1acbba4ae43fc1fb22e887a9
Simon Liang <no.lhr0909@gmail.com>
2020-08-05 13:48:20 +0800

    fixing project structure etc

--- src/stack.ts
+++ src/stack.ts
@@ -1,108 +1,97 @@
-import { TerraformStack, TerraformOutput } from "cdktf";
-import { Construct } from "constructs";
-import {
-	AwsProvider,
-	S3Bucket,
-	DataAwsIamPolicyDocument,
-	S3BucketPolicy,
-} from "../.gen/providers/aws";
+import { TerraformStack, TerraformOutput } from 'cdktf';
+import { Construct } from 'constructs';
+import { AwsProvider, S3Bucket, DataAwsIamPolicyDocument, S3BucketPolicy } from '../.gen/providers/aws';
 
 export class MyStack extends TerraformStack {
-	constructor(scope: Construct, name: string, resouceData: string) {
-		super(scope, name);
+  constructor(scope: Construct, name: string, resouceData: string) {
+    super(scope, name);
 
-        const resources = JSON.parse(resouceData);
-        
-        //TODO: use serverless options pass  the region
-		new AwsProvider(this, "provider", {
-			region: "us-west-2",
-		});
+    const resources = JSON.parse(resouceData);
 
-		let s3Bucket;
+    //TODO: use serverless options pass  the region
+    new AwsProvider(this, 'provider', {
+      region: 'us-west-2',
+    });
 
-		for (let key in resources) {
-			if (resources.hasOwnProperty(key)) {
-				console.log(resources[key].type);
+    let s3Bucket: S3Bucket | undefined;
 
-				if (resources[key].type === "AWS::S3::Bucket") {
-					const bucketProperties = resources[key].Properties;
+    for (const key in resources) {
+      if (Object.prototype.hasOwnProperty.call(resources, key)) {
+        console.log(resources[key].type);
 
-					//如果是 AWS::S3::Bucket
-					s3Bucket = new S3Bucket(this, key, {
-						serverSideEncryptionConfiguration: [
-							{
-								rule: [
-									{
-										applyServerSideEncryptionByDefault: [
-											{
-												sseAlgorithm:
-													bucketProperties.BucketEncryption[0]
-														.ServerSideEncryptionByDefault.SSEAlgorithm,
-											},
-										],
-									},
-								],
-							},
-						],
-					});
-				}
-			}
-		}
+        if (resources[key].type === 'AWS::S3::Bucket') {
+          const bucketProperties = resources[key].Properties;
 
-		for (let key in resources) {
-			if (resources.hasOwnProperty(key)) {
-				console.log(resources[key].type);
+          //如果是 AWS::S3::Bucket
+          s3Bucket = new S3Bucket(this, key, {
+            serverSideEncryptionConfiguration: [
+              {
+                rule: [
+                  {
+                    applyServerSideEncryptionByDefault: [
+                      {
+                        sseAlgorithm: bucketProperties.BucketEncryption[0].ServerSideEncryptionByDefault.SSEAlgorithm,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          });
+        }
+      }
+    }
 
-				if (s3Bucket === undefined) {
-					return;
-				}
+    for (const key in resources) {
+      if (Object.prototype.hasOwnProperty.call(resources, key)) {
+        console.log(resources[key].type);
 
-				if (resources[key].type === "AWS::S3::BucketPolicy") {
-                    const policyProperties = resources[key].Properties;
+        if (s3Bucket === undefined) {
+          return;
+        }
 
-                    const statement = policyProperties.PolicyDocument.Statement[0];
-                    const condition = statement.Condition
-                    //check condition keys
-                    const conditionKey = Object.keys(condition)[0]
-                    const conditionVariable = Object.keys(condition[conditionKey])[0];
+        if (resources[key].type === 'AWS::S3::BucketPolicy') {
+          const policyProperties = resources[key].Properties;
 
-					const s3Policy = new DataAwsIamPolicyDocument(
-						this,
-						"serverlessDeploymentBucketPolicyDocument",
-						{
-							statement: [
-								{
-									actions: [statement.Action],
-									effect: statement.Effect,
-									principals: [
-										{
-											identifiers: [statement.Principal],
-											type: "AWS",
-										},
-									],
-									resources: [`arn:aws:s3:::${s3Bucket.bucket}/*`],
-									condition: [
-										{
-											test: conditionKey,
-											variable: conditionVariable,
-											values: [condition[conditionKey][conditionVariable].toString()],
-										},
-									],
-								},
-							],
-						}
-					);
+          const statement = policyProperties.PolicyDocument.Statement[0];
+          const condition = statement.Condition;
+          //check condition keys
+          const conditionKey = Object.keys(condition)[0];
+          const conditionVariable = Object.keys(condition[conditionKey])[0];
 
-					new S3BucketPolicy(this, "serverlessDeploymentBucketPolicy", {
-						bucket: s3Bucket.bucket!,
-						policy: s3Policy.json,
-					});
+          const s3Policy = new DataAwsIamPolicyDocument(this, 'serverlessDeploymentBucketPolicyDocument', {
+            statement: [
+              {
+                actions: [statement.Action],
+                effect: statement.Effect,
+                principals: [
+                  {
+                    identifiers: [statement.Principal],
+                    type: 'AWS',
+                  },
+                ],
+                resources: [`arn:aws:s3:::${s3Bucket.bucket}/*`],
+                condition: [
+                  {
+                    test: conditionKey,
+                    variable: conditionVariable,
+                    values: [condition[conditionKey][conditionVariable].toString()],
+                  },
+                ],
+              },
+            ],
+          });
 
-					new TerraformOutput(this, "serverlessBucketName", {
-						value: s3Bucket.bucket,
-					});
-				}
-			}
-		}
-	}
+          new S3BucketPolicy(this, 'serverlessDeploymentBucketPolicy', {
+            bucket: s3Bucket.bucket ?? '',
+            policy: s3Policy.json,
+          });
+
+          new TerraformOutput(this, 'serverlessBucketName', {
+            value: s3Bucket.bucket,
+          });
+        }
+      }
+    }
+  }
 }
